1d169f62ce53bb80cd7bf042e2bc55b0075df663
YankaZabka <yanka.zanka@gmail.com>
2022-01-27 18:45:59 +0300

    Add exit game on wrong answer in brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,10 +2,11 @@
 import readlineSync from 'readline-sync';
 
 const evenGame = () => {
+  const NUMBER_OF_ROUNDS = 3
   const answers = [];
 
   const gameStep = (userName) => {
-    const number = Math.floor(Math.random() * 100);
+    const number = Math.floor(1 + Math.random() * 100);
     console.log(`Question: ${number}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === 'yes' && number % 2 === 0) {
@@ -17,6 +18,9 @@ const evenGame = () => {
     } else {
       console.log(`"${answer}" is a wrong answer ;(. Correct answer is "${answer === 'no' ? 'yes' : 'no'}"`);
       console.log(`Let's try again, ${userName}`);
+      while (answers.length < NUMBER_OF_ROUNDS) {
+        answers.push("wrong")
+      }
     }
   };
 
@@ -24,11 +28,11 @@ const evenGame = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\nAnswer "yes" if the number is even, otherwise answer "no".`);
 
-  while (answers.length < 3) {
+  while (answers.length < NUMBER_OF_ROUNDS && answers[answers.length - 1] !== "wrong") {
     gameStep(userName);
   }
 
-  console.log(`Congratulations, ${userName}!`);
+  answers[answers.length - 1] !== "wrong" ? console.log(`Congratulations, ${userName}!`) : null;
 };
 
 evenGame();
