21409a4064eed926b880ba26e902a30fb7ed4ab7
YankaZabka <yanka.zanka@gmail.com>
2022-01-28 14:01:47 +0300

    Do decomposition to evenGame

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,41 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import greeting from "../src/cli";
-
-const evenGame = () => {
-  const NUMBER_OF_ROUNDS = 3;
-  const answers = [];
-
-  const gameStep = (userName) => {
-    const number = Math.floor(1 + Math.random() * 100);
-    console.log(`Question: ${number}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === 'yes' && number % 2 === 0) {
-      answers.push(answer);
-      console.log('Correct!');
-    } else if (answer === 'no' && number % 2 !== 0) {
-      answers.push(answer);
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is a wrong answer ;(. Correct answer is "${answer === 'no' ? 'yes' : 'no'}"`);
-      console.log(`Let's try again, ${userName}`);
-      while (answers.length < NUMBER_OF_ROUNDS) {
-        answers.push('wrong');
-      }
-    }
-  };
-
-
-  const userName = greeting()
-  console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
-
-  while (answers.length < NUMBER_OF_ROUNDS && answers[answers.length - 1] !== 'wrong') {
-    gameStep(userName);
-  }
-
-  if (answers[answers.length - 1] !== 'wrong') {
-    console.log(`Congratulations, ${userName}!`);
-  }
-};
+import evenGame from "../games/evenGame.js";
 
 evenGame();
---
+++ games/evenGame.js
@@ -0,0 +1,24 @@
+import greeting from "../src/cli.js";
+import {gameStep, getRandomNumber, NUMBER_OF_ROUNDS} from "../src/index.js";
+
+const evenGame = () => {
+    const answers = [];
+
+    const userName = greeting()
+    console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+
+
+    while (answers.length < NUMBER_OF_ROUNDS && answers[answers.length - 1] !== 'GAME OVER') {
+        const question = getRandomNumber(1, 100)
+        const correctAnswer = question % 2 === 0 ? "yes" : "no"
+
+        const result = gameStep(userName, question, correctAnswer);
+        answers.push(result)
+    }
+
+    if (answers[answers.length - 1] !== 'GAME OVER') {
+        console.log(`Congratulations, ${userName}!`);
+    }
+};
+
+export default evenGame
