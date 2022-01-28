213f0405316f0463e30f021d60a57c47b983f3e1
YankaZabka <yanka.zanka@gmail.com>
2022-01-28 13:49:49 +0300

    Add index.js file with common games values and functions

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import readlineSync from "readline-sync";
+
+export const NUMBER_OF_ROUNDS = 3;
+
+export const getRandomNumber = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1)
+    return Math.floor(rand)
+}
+
+export const gameStep = (userName, question, correctAnswer) => {
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (answer === correctAnswer) {
+        console.log('Correct!');
+        return answer
+    } else {
+        console.log(`"${answer}" is a wrong answer ;(. Correct answer is "${correctAnswer}"`);
+        console.log(`Let's try again, ${userName}`);
+        return "GAME OVER"
+    }
+};
